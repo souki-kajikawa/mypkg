4f9e1a945521111dd3662fcf95f6b60c4ef15280
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-11-20 12:32:03 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='souki',
+    maintainer_email='s24c1038st@s.chibakoudai.jp',
+    description='a package for practice',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
