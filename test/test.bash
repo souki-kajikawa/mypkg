1988a296304986b8eb01a289298f594e3c2121c1
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-04 14:08:11 +0900

    Fix test

--- test/test.bash
+++ test/test.bash
@@ -5,8 +5,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+source $dir/install/setup.bash
+timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
 
 cat /tmp/mypkg.log |
 grep 'Listen: 10'
