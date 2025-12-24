a0faf0f80fcd3d8884a7d5edfa2ab081f2e11d20
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-24 16:13:52 +0900

    Fix

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,20 +2,27 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
-
-
-def cd():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
-
-
 def main():
-    node.create_timer(0.5, cd)
-    rclpy.spin(node)
+    rclpy.init()
+    node = Node("talker")
+    pub = node.create_publisher(Int16, "countup", 10)
+    n = 0
+
+    def timer_callback():
+        nonlocal n
+        msg = Int16()
+        msg.data = n
+        pub.publish(msg)
+        n += 1
+
+    node.create_timer(0.5, timer_callback)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
