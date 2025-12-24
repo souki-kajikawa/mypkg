4164a38145acc00615097af333e2b918d3a56550
souki-kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-24 16:47:46 +0900

    Update listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,23 +2,16 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+
+rclpy.init()
+node = Node("listener")
+
+
+def cd(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+
 def main():
-    rclpy.init()
-    node = Node("listener")
-
-    def cb(msg):
-        print(f"Listen: {msg.data}")
-        node.get_logger().info(f"Listen: {msg.data}")
-
-    node.create_subscription(Int16, "countup", cb, 10)
-    
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+    pub = node.create_subscription(Int16, "countup", cd, 10)
+    rclpy.spin(node)
