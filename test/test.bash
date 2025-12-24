936c63fad5eedff35b9f93ad25a83cad01d38768
souki-kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-24 16:49:38 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,17 +1,13 @@
 #!/bin/bash
-dir=/root
+
+dir=~
+
+[ "$1" != "" ] && dir="$1"
+
 cd $dir/ros2_ws
-rm -rf build/ install/ log/
 colcon build
-source /opt/ros/humble/setup.bash
-source install/local_setup.bash
-timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
-sleep 2
-cat /tmp/mypkg.log
-if grep -q 'Listen: 10' /tmp/mypkg.log; then
-    echo "SUCCESS"
-    exit 0
-else
-    echo "FAILURE"
-    exit 1
-fi
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
