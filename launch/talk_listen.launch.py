cc5c83f7ef2772a7ec0360c94fdfa00abec2748d
souki-kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-24 16:46:21 +0900

    Update talk_listen.launch.py

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,27 +1,19 @@
 import launch
 import launch.actions
-<<<<<<< HEAD
-import launch.substitution
-=======
 import launch.substitutions
->>>>>>> lesson10
 import launch_ros.actions
 
 
 def generate_launch_description():
 
     talker = launch_ros.actions.Node(
-            package='mypkg',
-            executable='talker',
-            )
+        package='mypkg',      
+        executable='talker',  
+        )
     listener = launch_ros.actions.Node(
-<<<<<<< HEAD
-            package='mypkg',
-            executable='listener',
-=======
-            package='mypkg'
-            executable='listener'
->>>>>>> lesson10
-            output='screen'
-            )
-    return launch.LaunchDescription([talker, listener])
+        package='mypkg',
+        executable='listener',
+        output='screen'                       
+        )
+ 
+    return launch.LaunchDescription([talker, listener]) 
