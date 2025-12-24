8b54ff5a3cb42bad064745bf439513ced57955c7
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-24 15:51:10 +0900

    Fix

--- test/test.bash
+++ test/test.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-dir=~
-[ "$1" != "" ] && dir="$1"
+dir=/root
 cd $dir/ros2_ws
+rm -rf build/ install/ log/
 colcon build
-source $dir/.bashrc
-timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log 
-grep 'Listen: 10'
+source /opt/ros/humble/setup.bash
+source install/local_setup.bash
+timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
+cat /tmp/mypkg.log | grep 'Listen: 10'
