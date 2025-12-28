cbe614e58fc4978812f8354d37e5ad982141ad67
souki kajikawa <s24c1038st@s.chibakoudai.jp>
2025-12-28 16:06:53 +0900

    Updata

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,18 +3,16 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-
+from std_msgs.msg import UInt64
 
 rclpy.init()
 node = Node("listener")
 
-
 def cd(msg):
     global node
-    node.get_logger().info("Listen: %d" % msg.data)
+    node.get_logger().info("receive: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cd, 10)
+    pub = node.create_subscription(UInt64, "receive", cd, 10)
     rclpy.spin(node)
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,24 +1,29 @@
 # SPDX-FileCopyrightText: 2025 Souki Kajikawa
 # SPDX-License-Identifier: BSD-3-Clause
 
+import psutil
 import rclpy
+import time
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import UInt64
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+pub = node.create_publisher(UInt64, "receive", 10)
+
 
 
 def cd():
-    global n
-    msg = Int16()
+    n = 0
+    rec0 = psutil.net_io_counters().bytes_recv
+    time.sleep(2.9)
+    rec1 = psutil.net_io_counters().bytes_recv
+    n = rec1 - rec0
+    msg = UInt64()
     msg.data = n
     pub.publish(msg)
-    n += 1
 
 
 def main():
-    node.create_timer(0.5, cd)
+    node.create_timer(0.1, cd)
     rclpy.spin(node)
